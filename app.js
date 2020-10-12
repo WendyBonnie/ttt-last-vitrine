cc5ec5d9e91c398efe4f91b149bff1cbd62702fd
Wendy Montagnon <wendymontagnon@gmail.com>
2020-10-12 10:38:32 +0200

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * App
+ */
+
+/* Imports */
+const express = require('express');
+const logger = require('morgan');
+const path = require("path");
+
+/* App creation */
+let app = express();
+
+/* Middlewares */
+app.use(logger('dev'));
+
+/* Serve static files */
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* Handle routes for react router */
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+
+module.exports = app;
---
+++ public/precache-manifest.8934d76c9fa96213a4327cac11fe03cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0cbf89034d519841a9c1d5dce92ed8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7df2914ac24209c8f7b",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e1788506e3519277f130",
+    "url": "/static/css/main.77367f6f.chunk.css"
+  },
+  {
+    "revision": "a7df2914ac24209c8f7b",
+    "url": "/static/js/2.86a5d44b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.86a5d44b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1788506e3519277f130",
+    "url": "/static/js/main.08a54048.chunk.js"
+  },
+  {
+    "revision": "f172093a2682716b29e2",
+    "url": "/static/js/runtime-main.5cf18ba1.js"
+  }
+]);
