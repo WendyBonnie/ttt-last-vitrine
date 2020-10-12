ab2965664359ddb65f05660014968c12bcb89f40
Wendy Montagnon <wendymontagnon@gmail.com>
2020-10-12 10:45:59 +0200

    commit

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 /**
- * App
+ ** * App
  */
 
 /* Imports */
-const express = require('express');
-const logger = require('morgan');
+const express = require("express");
+const logger = require("morgan");
 const path = require("path");
 
 /* App creation */
 let app = express();
 
 /* Middlewares */
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 /* Serve static files */
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 /* Handle routes for react router */
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
-
 
 module.exports = app;
