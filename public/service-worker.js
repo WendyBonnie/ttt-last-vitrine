6b25b93ea0c6751b8a36b61df16de1f1794d9e24
wendy <wendymontagnon@gmail.com>
2020-12-16 11:54:15 +0100

    cgu

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3875c6c57608a5fa4d6e368c34674288.js"
+  "/precache-manifest.804fd4a69558f25720a09e192b816f08.js"
 );
 
 self.addEventListener('message', (event) => {
