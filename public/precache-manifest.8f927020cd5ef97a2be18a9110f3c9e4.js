685661d76724c98e1cd390fbbad3804818dc66b7
Wendy-M <wendymontagnon@gmail.com>
2021-06-03 15:40:08 +0200

    update

---
+++ public/precache-manifest.8f927020cd5ef97a2be18a9110f3c9e4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c9003adb4f5403db06f340b7287322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "101e2c6a43df0817f260",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "da53f42ce5ada4d1cf8a",
+    "url": "/static/css/main.38c5c38d.chunk.css"
+  },
+  {
+    "revision": "101e2c6a43df0817f260",
+    "url": "/static/js/2.80404031.chunk.js"
+  },
+  {
+    "revision": "9154cbd0563ac6f79c6a1f609d0543aa",
+    "url": "/static/js/2.80404031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da53f42ce5ada4d1cf8a",
+    "url": "/static/js/main.228bd928.chunk.js"
+  },
+  {
+    "revision": "f172093a2682716b29e2",
+    "url": "/static/js/runtime-main.5cf18ba1.js"
+  },
+  {
+    "revision": "f4d875de4ba0822ca2e4836aa9b57941",
+    "url": "/static/media/CLIENTE.f4d875de.mp4"
+  },
+  {
+    "revision": "f6378502a37368fba716abff422d7681",
+    "url": "/static/media/PATRON.f6378502.mp4"
+  },
+  {
+    "revision": "c0292a22c1b9983b7b7bc92cad3689a7",
+    "url": "/static/media/SERVEUR.c0292a22.mp4"
+  },
+  {
+    "revision": "b2acaf2973fd98275bf7353289883275",
+    "url": "/static/media/fond.b2acaf29.png"
+  }
+]);
--- public/service-worker 2.js
+++ public/service-worker 2.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f927020cd5ef97a2be18a9110f3c9e4.js"
+  "/precache-manifest.b08eaa81f89127feddb032a3e640c23d.js"
 );
 
 self.addEventListener('message', (event) => {
