fc16a30fd1c07bc519c891911d49f871989ae692
Wendy Montagnon <wendymontagnon@gmail.com>
2022-02-21 13:34:48 +0000

    majk

---
+++ public/precache-manifest.264a27dce04fed8ffb5f2b6abbeb1625.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687cffc5e7f18ad7ebbe1a2d5b25c694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89e47ebf6ef0150542ec",
+    "url": "/static/css/2.055bdf39.chunk.css"
+  },
+  {
+    "revision": "2ccef2a00b03804b4731",
+    "url": "/static/css/main.29cf1a29.chunk.css"
+  },
+  {
+    "revision": "89e47ebf6ef0150542ec",
+    "url": "/static/js/2.16240e25.chunk.js"
+  },
+  {
+    "revision": "92b2cb9bd8b7720b1ddd4e333c8952f2",
+    "url": "/static/js/2.16240e25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ccef2a00b03804b4731",
+    "url": "/static/js/main.5afde17d.chunk.js"
+  },
+  {
+    "revision": "f172093a2682716b29e2",
+    "url": "/static/js/runtime-main.5cf18ba1.js"
+  },
+  {
+    "revision": "f4d875de4ba0822ca2e4836aa9b57941",
+    "url": "/static/media/CLIENTE.f4d875de.mp4"
+  },
+  {
+    "revision": "f6378502a37368fba716abff422d7681",
+    "url": "/static/media/PATRON.f6378502.mp4"
+  },
+  {
+    "revision": "c0292a22c1b9983b7b7bc92cad3689a7",
+    "url": "/static/media/SERVEUR.c0292a22.mp4"
+  },
+  {
+    "revision": "b2acaf2973fd98275bf7353289883275",
+    "url": "/static/media/fond.b2acaf29.png"
+  }
+]);
